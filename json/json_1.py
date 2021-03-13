51c1e8272ea6e4317fa9ad89a91d6b9a167b31f8
AT-202007022209\Administrator <pongharit.me@ku.th>
2021-03-14 02:01:27 +0700

    new

---
+++ json/json_1.py
@@ -0,0 +1,13 @@
+import json
+
+# some JSON:
+json_string = '{ "name": "Pongharit & Arm", "age":21.5,"city":"Thai"}'
+
+# parse x:
+python_dict = json.loads(json_string)
+
+# the result is a Python dictionary:
+print(python_ditc["name"])
+print(python_ditc["age"])
+print(python_ditc["city"])
+© 2021 GitHub, Inc.
---
+++ json/json_2.py
@@ -0,0 +1,10 @@
+import json
+
+# a Python object (dict):
+python_dict = {"name": "Pongharit & Arm", "age": 21.5, "city": "Thai"}
+
+# convert into JSON:
+json_string = json.dump(python_dict)
+
+# the result is a JSON string:
+print(json_string)
